a8e024f8add42ea6477c7acf53705068667e93a2
EK <evren.kass@gmail.com>
2021-12-17 16:18:19 +0400

    failed test

--- src/home.js
+++ src/home.js
@@ -2,7 +2,7 @@ import { Card } from "./Card";
 
 export function Home() {
   return (
-    <Card header="Dreambank Landing Module" className="text-center">
+    <Card header="Dreambank Land Module" className="text-center">
       <p className="mt-4">Welcome to your online banking page</p>
       <p>You can move around using the navigation bar</p>
       <img src="bank.png" className="img-fluid w-50" alt="" />
