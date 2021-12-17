f348c0507205d4928d9aa595bcfe67c615aa14af
EK <evren.kass@gmail.com>
2021-12-17 16:34:03 +0400

    fixed testing 2

--- src/pup.test.js
+++ src/pup.test.js
@@ -4,19 +4,19 @@ const http = require("http");
 const server = http.createServer((request, response) => {
   return handler(request, response, { public: "build" });
 });
+let browser;
 afterAll(() => {
+  browser.close();
   server.close();
 });
 beforeAll(() => {
+  browser = await puppeteer.launch();
   server.listen(3001);
 });
 it("checks title", async () => {
-  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("http://localhost:3001");
   const title = await page.$(".card-title");
   const text = await (await title.getProperty("textContent")).jsonValue();
   expect(text).toEqual("Dreambank Landing Module");
-
-  await browser.close();
 });
