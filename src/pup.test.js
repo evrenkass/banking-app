7c21c9b68e267bfbe94e51c9508c767b416bae36
EK <evren.kass@gmail.com>
2021-12-17 16:28:10 +0400

    fixed testing

--- src/pup.test.js
+++ src/pup.test.js
@@ -1,26 +1,22 @@
 const puppeteer = require("puppeteer");
 const handler = require("serve-handler");
 const http = require("http");
-it("checks title", () => {
-  const server = http.createServer((request, response) => {
-    return handler(request, response, { public: "build" });
-  });
+const server = http.createServer((request, response) => {
+  return handler(request, response, { public: "build" });
+});
+afterAll(() => {
+  server.close();
+});
+beforeAll(() => {
+  server.listen(3001);
+});
+it("checks title", async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("http://localhost:3001");
+  const title = await page.$(".card-title");
+  const text = await (await title.getProperty("textContent")).jsonValue();
+  expect(text).toEqual("Dreambank Landing Module");
 
-  server.listen(3001, () => {
-    (async () => {
-      try {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto("http://localhost:3001");
-        const title = await page.$(".card-title");
-        const text = await (await title.getProperty("textContent")).jsonValue();
-        expect(text).toEqual("Dreambank Landing Module");
-
-        await browser.close();
-        server.close();
-      } catch {
-        server.close();
-      }
-    })();
-  });
+  await browser.close();
 });
