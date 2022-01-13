941fd9eec8209581e263252beec2a06d0bb12036
EK <evren.kass@gmail.com>
2022-01-13 12:18:46 +0400

    Add image in authorizing page

--- client/src/CredentialsLoading.js
+++ client/src/CredentialsLoading.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export const CredentialsLoading = () => {
-  return <div>Loading credentials</div>;
+  return (
+    <div className="d-flex justify-content-center align-items-center w-100 vh-100">
+      <div className="w-50 text-center">
+        <img src="/authorizing.svg" alt="Authorizing" className="img-fluid" />
+        <h2 className="mt-2">Authorizing</h2>
+      </div>
+    </div>
+  );
 };
